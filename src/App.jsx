46492c3f0e9245bc5891a3dfcc6efad276fb49c6
Mikhail Mogilnikov <mikhail.mogilnikov02@gmail.com>
2023-10-13 22:55:21 +0300

    fix: Routes.jsx export

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import { ProjectRoutes } from './Routes'
-import { Footer } from './components/footer/Footer'
-import { Header } from './components/header/Header'
-import UseToTop from './hooks/useToTop'
-import './styles/main.scss'
+import ProjectRoutes from './Routes.jsx';
+import { Footer } from './components/footer/Footer';
+import { Header } from './components/header/Header';
+import UseToTop from './hooks/useToTop';
+import './styles/main.scss';
 
 function App() {
-	return (
-		<div className='main-wrapper'>
-			<UseToTop />
+  return (
+    <div className="main-wrapper">
+      <UseToTop />
 
-			<Header />
-			<ProjectRoutes />
-			<Footer />
-		</div>
-	)
+      <Header />
+      <ProjectRoutes />
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
